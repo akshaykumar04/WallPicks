274e44c061a343f97bca597e8b373717b9b72700
Akshay kumar <mail.akshay04@gmail.com>
2017-09-08 23:24:36 +0530

    -

---
+++ app/src/main/java/com/akshay/wallpicks/activities/MuzeiActivity.java
@@ -0,0 +1,15 @@
+package com.akshay.wallpicks.activities;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import com.dm.wallpaper.board.activities.WallpaperBoardMuzeiActivity;
+import com.akshay.wallpicks.services.MuzeiService;
+
+public class MuzeiActivity extends WallpaperBoardMuzeiActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        initMuzeiActivity(savedInstanceState, MuzeiService.class);
+    }
+}
